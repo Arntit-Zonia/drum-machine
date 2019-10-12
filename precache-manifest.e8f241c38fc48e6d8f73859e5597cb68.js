1e05c45f771e1613629717db317cb5f201069b6a
Arntit-Zonia <arditzonia@gmail.com>
2019-10-12 14:34:30 +0100

    Updates

---
+++ precache-manifest.e8f241c38fc48e6d8f73859e5597cb68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ede2b3f6d3611cdfc55e95154cab1ac",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "8c5c12ee85e7c9040587",
+    "url": "/drum-machine/static/css/main.8eab5044.chunk.css"
+  },
+  {
+    "revision": "4535ed1161fd0ebfa0d7",
+    "url": "/drum-machine/static/js/2.a068997d.chunk.js"
+  },
+  {
+    "revision": "8c5c12ee85e7c9040587",
+    "url": "/drum-machine/static/js/main.b6f4d245.chunk.js"
+  },
+  {
+    "revision": "f26c23cded879efdcd24",
+    "url": "/drum-machine/static/js/runtime-main.bb8f9f46.js"
+  }
+]);
